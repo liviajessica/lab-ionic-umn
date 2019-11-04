e2ed3f0e082b339cc2bff33f603e156ac19738a8
Livia Jessica <liviajessica1503@gmail.com>
2019-11-05 06:10:08 +0700

    update week 10 yang tutorial firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCIqObxJwiUlDVn9ptjUJ3dgNIvGejV-1g",
+    authDomain: "week10-57eb0.firebaseapp.com",
+    databaseURL: "https://week10-57eb0.firebaseio.com",
+    projectId: "week10-57eb0",
+    storageBucket: "week10-57eb0.appspot.com",
+    messagingSenderId: "1088878571501",
+    appId: "1:1088878571501:web:23d0ccee27fea4b4c6c1e4",
+    measurementId: "G-229K83SZSF"
+  }
 };
 
 /*
